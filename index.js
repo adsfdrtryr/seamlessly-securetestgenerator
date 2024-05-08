edd364642bd9a20067fa815bff8f94064f6b8dfe
adsfdrtryr <msjcurdock.live@hotmail.com>
2024-05-09 01:53:50 +0700

    hello commit

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-function mergeKLists(lists) {
-  if (!lists || !lists.length) return null;
-  return mergeLists(lists, 0, lists.length - 1);
-  function mergeLists(lists, start, end) {
-    if (start === end) return lists[start];
-    const mid = Math.floor((start + end) / 2);
-    const left = mergeLists(lists, start, mid);
-    const right = mergeLists(lists, mid + 1, end);
-    return mergeTwoLists(left, right);
+function canFinish(numCourses, prerequisites) {
+  const graph = new Array(numCourses).fill(0).map(() => []);
+  const inDegree = new Array(numCourses).fill(0);
+  for (const [course, pre] of prerequisites) {
+    graph[pre].push(course);
+    inDegree[course]++;
   }
-  function mergeTwoLists(l1, l2) {
-    if (!l1) return l2;
-    if (!l2) return l1;
-    if (l1.val < l2.val) {
-      l1.next = mergeTwoLists(l1.next, l2);
-      return l1;
-    } else {
-      l2.next = mergeTwoLists(l1, l2.next);
-      return l2;
+  const queue = [];
+  for (let i = 0; i < numCourses; i++) {
+    if (inDegree[i] === 0) queue.push(i);
+  }
+  let count = 0;
+  while (queue.length) {
+    const course = queue.shift();
+    count++;
+    for (const nextCourse of graph[course]) {
+      inDegree[nextCourse]--;
+      if (inDegree[nextCourse] === 0) queue.push(nextCourse);
     }
   }
+  return count === numCourses;
 }
